4943bae14c77e4b4247d561c99d95d182692623f
Jules <60539590+JulesG10@users.noreply.github.com>
2021-04-18 20:09:47 +0200

    8.2.5

--- src/app/server-service/server-service.component.ts
+++ src/app/server-service/server-service.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ServerServiceComponent {
 
-  private APP_VERSION: string = "8.0.5";
+  private APP_VERSION: string = "8.2.5";
   private ServerURL: string = 'https://send-server-api.herokuapp.com/';
   
   private socket: any;
